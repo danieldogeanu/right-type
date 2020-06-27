74f0dbeeb04b352ee950d19c1c01c6155c62f9b1
Daniel Dogeanu <contact@danieldogeanu.com>
2020-06-27 21:46:29 +0300

    Added Helper Function to Run Batch Type Tests

--- src/helper.spec.js
+++ src/helper.spec.js
@@ -1,4 +1,10 @@
+import {expect} from 'chai'
+
 // Helper Functions for Tests
+// NOTE: We named this file .spec.js because we don't want it included in the final package.
+// -----------------------------------------------------------------------------
+
+
 
 /**
  * Function that makes fancy titles for tests.
@@ -7,13 +13,47 @@
  * @returns {string} Returns a fancy title string.
  */
 export function makeTitle(title) {
-  const dash = '-';
-  const line = dash.repeat(50 - title.split('').length);
-  return `${title} ${line}\n`;
+  const dash = '-'
+  const line = dash.repeat(50 - title.split('').length)
+  return `${title} ${line}\n`
+}
+
+
+/**
+ * Function that creates and runs batch tests for all primitive JavaScript types.
+ * @param {string} type The name of the type to return true for.
+ * @param {function} method The function/method to batch tests against.
+ */
+export function runBatchTypeTests(type, method) {
+  if (typeof type !== 'string' && typeof method !== 'function') return
+
+  const typeMatcher = (passed, expected) => (passed === expected)
+  const runTestFor = (chosenType, testValue) => {
+    it(`returns ${typeMatcher(type, chosenType)} for ${chosenType}`, () => {
+      expect(method(testValue)).to.equal(typeMatcher(type, chosenType))
+    })
+  }
+
+  const allTypes = {
+    undefined: undefined,
+    boolean: true,
+    number: 543,
+    string: 'test string',
+    bigint: 23n,
+    symbol: Symbol('Test'),
+    array: [1, 2, 3],
+    object: {one: 1, two: 2, three: 3},
+    function: () => true,
+  }
+
+  Object.keys(allTypes).forEach(type => {
+    runTestFor(type, allTypes[type])
+  })
 }
 
 // -----------------------------------------------------------------------------
 
 export default {
   makeTitle,
+  runBatchTypeTests,
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,38 +1,12 @@
 import {expect} from 'chai'
-import {makeTitle} from './helper.spec'
+import {makeTitle, runBatchTypeTests} from './helper.spec'
 import rightType from './index'
 
 describe(makeTitle('Right Type'), () => {
 
   describe('isString', () => {
 
-    it('returns true for string', () => {
-      expect(rightType.isString('test string')).to.equal(true)
-    })
-
-    it('returns false for empty string', () => {
-      expect(rightType.isString('')).to.equal(false)
-    })
-
-    it('returns true for empty string with the empty flag set', () => {
-      expect(rightType.isString('', true)).to.equal(true)
-    })
-
-    it('returns false for number', () => {
-      expect(rightType.isString(5)).to.equal(false)
-    })
-
-    it('returns false for array', () => {
-      expect(rightType.isString([])).to.equal(false)
-    })
-
-    it('returns false for function', () => {
-      expect(rightType.isString(() => {})).to.equal(false)
-    })
-
-    it('returns false for object', () => {
-      expect(rightType.isString({})).to.equal(false)
-    })
+    runBatchTypeTests('string', rightType.isString)
 
   })
 
