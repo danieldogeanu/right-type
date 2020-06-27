80807790098508f094f047b5647d0a15e65bb6df
Daniel Dogeanu <contact@danieldogeanu.com>
2020-06-27 22:39:09 +0300

    Changed Empty Param Name

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@
  * Check to see if the value provided is a string.
  * By default it also checks to see if the string is empty and returns false if it is.
  * @param {string} string The value to be tested.
- * @param {boolean} empty Whether to allow empty strings or not.
+ * @param {boolean} allowEmpty Whether to allow empty strings or not.
  */
-export function isString(string, empty = false) {
-  return (empty) ? (typeof string === 'string') :
+export function isString(string, allowEmpty = false) {
+  return (allowEmpty) ? (typeof string === 'string') :
     (typeof string === 'string' && string !== '')
 }
 
@@ -20,10 +20,10 @@ export function isString(string, empty = false) {
  * Check to see if the value provided is a number.
  * By default it also checks to see it the number is not zero and returns false if it is.
  * @param {number} number The value to be tested.
- * @param {boolean} empty Whether to allow zero or not.
+ * @param {boolean} allowEmpty Whether to allow zero or not.
  */
-export function isNumber(number, empty = false) {
-  return (empty) ? (typeof number === 'number' && !isNaN(number)) :
+export function isNumber(number, allowEmpty = false) {
+  return (allowEmpty) ? (typeof number === 'number' && !isNaN(number)) :
     (typeof number === 'number' && !isNaN(number) && number !== 0)
 }
 
